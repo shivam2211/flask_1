6f8ad00aaf2d92f278867ba2059c84c50af84b5e
Shivam <shivamformal@gmail.com>
2019-08-16 10:39:29 +0530

    Initial Commit

---
+++ app/api/tokens.py
@@ -0,0 +1,16 @@
+from flask import jsonify, g, request
+from app import db
+from app.api import bp
+from app.api.auth import basic_auth
+
+@bp.route('/tokens', methods=['POST'])
+@basic_auth.login_required
+def get_token():
+
+    token = g.current_user.get_token()
+    db.session.commit()
+    return jsonify({'token': token, 'userid': g.current_user.id})
+
+def revoke_token():
+    pass
+
